f26daa8689f3fab472522484e6c9397c33375ba7
arodpes <arodpes@gmail.com>
2021-05-21 10:36:54 +0100

    segundo

--- src/main/java/com/arodpes/prueba04/App.java
+++ src/main/java/com/arodpes/prueba04/App.java
@@ -17,12 +17,12 @@ public class App
 	public static int suma(int num1, int num2) {
 		return num1+num2;
 	}
-	
+	/*
 	public static int suma2(Integer num1, Integer num2) {
 		num2= null;
 		return num1.intValue()+num2.intValue();
 	}
-	
+	*/
     public static void main( String[] args )
     {
         //System.out.println((suma(8,4)));
