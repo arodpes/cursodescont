1c7bfc815f28ce8cf1ee2d5526e65fb923209ca2
arodpes <arodpes@gmail.com>
2021-05-24 10:32:08 +0100

    actualizar prueba con textos

--- src/main/java/com/arodpes/prueba04/Menu.java
+++ src/main/java/com/arodpes/prueba04/Menu.java
@@ -6,9 +6,9 @@ public class Menu {
 		int elemento1 = 100;
 		int elemento2= 12;
 		System.out.println("la suma es " + App.suma(elemento1, elemento2));
-		System.out.println("la resta es " + Operaciones.resta(elemento1, elemento2));
-		System.out.println("la multiplicación es " + Operaciones.multiplicacion(elemento1, elemento2));
-		System.out.println("la división es " + Operaciones.division(elemento1, elemento2));
+		System.out.println("la diferencia es " + Operaciones.resta(elemento1, elemento2));
+		System.out.println("el producto es " + Operaciones.multiplicacion(elemento1, elemento2));
+		System.out.println("el cociente es " + Operaciones.division(elemento1, elemento2));
 
 	}
 
